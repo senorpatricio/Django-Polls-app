8d5a40c90d6bc4f546d4e37e2ecefc6ede75e758
Patrick Harding <patrickharding@patrickharding.local>
2016-01-11 11:40:58 -0700

    updated admin page and added custom admin template
    added search function, and filter-by-date function to admin page on
    backend.

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+# 	fields = ['pub_date', 'question_text']
+
+# admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
+
+class ChoiceInLine(admin.TabularInline):  # can also be StackedInline
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,					{'fields': ['question_text']}),
+		('Date Information',	{'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInLine]
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']  # adds ability to filter by publishing date
+	search_fields = ['question_text']  # adds a search box to search through questions
+
+admin.site.register(Question, QuestionAdmin)
+# This tells Django: Choice objects are edited on the Question admin page. 
+# By default, provide enough fields for 3 choices.
